2bb384892ce51c7bb6e3a1886d3b093ef97e2463
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-15 00:29:42 +0200

    Added header license comment and credits in files

--- resources/assets/js/views/IndexView.js
+++ resources/assets/js/views/IndexView.js
@@ -1,3 +1,10 @@
+/**
+ * LayanOS
+ *
+ * @link      https://github.com/sdanielch/lyos
+ * @copyright Copyright (c) 2017 LayanOS
+ * @license   https://github.com/sdanielch/lyos/blob/master/README.md (GPL-3.0 License)
+ */
 import $ from 'jquery';
 import handlebars from 'handlebars';
 import Backbone from 'backbone';
