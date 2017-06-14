c239934858e6ddfa33c44bca90ae07f6055978b3
Siro Díaz Palazón <sirodiaz93@gmail.com>
2017-06-15 00:23:47 +0200

    Minification for production environment and defined headers comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+/**
+ * LayanOS
+ *
+ * @link      https://github.com/sdanielch/lyos
+ * @copyright Copyright (c) 2017 LayanOS
+ * @license   https://github.com/sdanielch/lyos/blob/master/README.md (GPL-3.0 License)
+ */
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: path.resolve(__dirname, 'resources', 'assets', 'js', 'app.js'),
@@ -8,6 +16,10 @@ module.exports = {
         filename: 'app.js',
         path: path.resolve(__dirname, 'public', 'js')
     },
+    plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })],
     module: {
         loaders: [{
             test: /.js$/,
